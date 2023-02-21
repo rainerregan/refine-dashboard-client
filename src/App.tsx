57279439db4d6957a66ecc710af11bf6b19e6e93
Rainer Regan <rainercoc@gmail.com>
2023-02-21 10:53:38 +0700

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,36 @@ import {
   ErrorComponent,
 } from "@pankod/refine-mui";
 
+import {
+  AccountCircleOutlined,
+  ChatBubbleOutline,
+  PeopleAltOutlined,
+  StarOutlineRounded,
+  VillaOutlined
+} from '@mui/icons-material';
+
 import dataProvider from "@pankod/refine-simple-rest";
 import { MuiInferencer } from "@pankod/refine-inferencer/mui";
 import routerProvider from "@pankod/refine-react-router-v6";
 import axios, { AxiosRequestConfig } from "axios";
 import { ColorModeContextProvider } from "contexts";
 import { Title, Sider, Layout, Header } from "components/layout";
-import { Login } from "pages/login";
 import { CredentialResponse } from "interfaces/google";
 import { parseJwt } from "utils/parse-jwt";
+
+import { 
+  Login,
+  Home,
+  Agents,
+  MyProfile,
+  PropertyDetails,
+  AllProperties,
+  CreateProperty,
+  AgentProfile,
+  EditProperty,
+} from "pages";
+
+import './index.css';
 
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
@@ -98,12 +119,36 @@ function App() {
           catchAll={<ErrorComponent />}
           resources={[
             {
-              name: "posts",
-              list: MuiInferencer,
-              edit: MuiInferencer,
-              show: MuiInferencer,
-              create: MuiInferencer,
-              canDelete: true,
+              name: "properties",
+              list: AllProperties,
+              show: PropertyDetails,
+              create: CreateProperty,
+              edit: EditProperty,
+              icon: <VillaOutlined />
+            },
+            {
+              name: "agents",
+              list: Agents,
+              show: AgentProfile,
+              icon: <PeopleAltOutlined />
+            },
+            {
+              name: "reviews",
+              list: Home,
+              icon: <StarOutlineRounded />
+            },
+            {
+              name: "messages",
+              list: Home,
+              icon: <ChatBubbleOutline />
+            },
+            {
+              name: "my-profile",
+              options: {
+                label: "My Profile"
+              },
+              list: MyProfile,
+              icon: <AccountCircleOutlined />
             },
           ]}
           Title={Title}
@@ -113,6 +158,7 @@ function App() {
           routerProvider={routerProvider}
           authProvider={authProvider}
           LoginPage={Login}
+          DashboardPage={Home}
         />
       </RefineSnackbarProvider>
     </ColorModeContextProvider>
